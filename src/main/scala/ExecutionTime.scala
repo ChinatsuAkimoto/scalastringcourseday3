c353dc7963c5954c31b649c3607942f5b0500fc2
ktr-skmt <sakamoto_911@hotmail.com>
2016-01-26 07:09:12 +0900

    update

--- src/main/scala/ExecutionTime.scala
+++ src/main/scala/ExecutionTime.scala
@@ -1,40 +1,50 @@
+
+object ExecutionTime {
+  /**
+    * 処理の平均実行時間をミリ秒単位で標準出力
+    */
+  def printlnAverageExecutionTime(executionTime: Long, n: Int): Unit = {
+    printf("%d [nano seconds] (the average of %,d times)%n", executionTime, n)
+  }
+}
+
 /**
   * 処理の実行時間計測
+  *
   * @param n 試行回数
   */
 class ExecutionTime(val n: Int) {
   /**
     * 処理の平均実行時間をミリ秒単位で標準出力
+    *
     * @param proc 処理
     */
   def printlnAverageExecutionTime(proc : => Unit): Unit = {
-    val executionTime: Double =
-      if (n <= 1) {
-        getExecutionTime(proc)
-      } else {
-        getAverageExecutionTime(proc)
-      }
-    val nanoSeconds: Long = math.round(executionTime)
-    printf("%,d [nano seconds] (the average of %,d times)%n", nanoSeconds, n)
+    ExecutionTime.printlnAverageExecutionTime(getAverageExecutionTime(proc), n)
   }
 
   /**
     * 処理の平均実行時間を測定
+    *
     * @param proc 処理
     * @return 処理の平均実行時間
     */
-  def getAverageExecutionTime(proc : => Unit): Double = {
+  def getAverageExecutionTime(proc : => Unit): Long = {
     //オーバーフロー対策としてBigIntを使用
     var total: BigInt = BigInt(0)
-    for (i <- 0 until n) {
-      total += getExecutionTime(proc)
+    Iterator.range(0, n + 10).foreach {
+      i =>
+        if (10 < i) {
+          total += getExecutionTime(proc)
+        }
     }
     //平均化で浮動小数を使用するためにBigDecimalに変換し、平均化後、Doubleに変換
-    (BigDecimal(total) / n).toDouble
+    math.round((BigDecimal(total) / n).toDouble)
   }
 
   /**
     * 実行時間を測定
+    *
     * @param proc 処理
     * @return 処理の実行時間
     */
